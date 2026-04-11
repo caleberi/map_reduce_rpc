bcb5439cee444863d2349f865c90b1e1ed65d431
Caleb Adewole Erioluwa <caleberioluwa@gmail.com>
2026-04-11 22:32:30 +0100

    refactor: remove unused coordinator and worker files

--- mr/notify_intermediate_file.go
+++
@@ -1,8 +0,0 @@
-package mr
-
-func (coord *Coordinator) NotifyIntermediateFile(args *NotifyIntermediateArgs, reply *NotifyReply) error {
-	coord.rWMutexLock.Lock()
-	defer coord.rWMutexLock.Unlock()
-	coord.intermediateFiles[args.ReduceIndex] = append(coord.intermediateFiles[args.ReduceIndex], args.File)
-	return nil
-}
